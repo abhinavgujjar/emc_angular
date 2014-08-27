b7a81c5412c0c841fa2a068f2dc5886528a01d50
Abhinav Gujjar <abhinavgujjar@gmail.com>
2014-08-27 09:52:02 +0530

    services

--- Basic/app.js
+++ Basic/app.js
@@ -1,69 +1,35 @@
 angular.module('ha', []);
 
-angular.module('ha').controller('mainController', ['$scope',
-	function($scope) {
+angular.module('ha').controller('mainController', ['$scope', 
+	'constants', 'hotelsProvider',
+	function($scope, constants, hp) {
 		$scope.greeting = 'Hello EMC2';
+		$scope.descLimit = constants.descLimit;
+		$scope.maxResults = constants.maxResults;
 
-		$scope.upVote = function(hotel){
-			if (!hotel.rating){
+		$scope.upVote = function(hotel) {
+			if (!hotel.rating) {
 				hotel.rating = 0;
 			}
 
 			hotel.rating++;
 		}
 
-		$scope.downVote= function(hotel){
-			if (!hotel.rating){
+		$scope.downVote = function(hotel) {
+			if (!hotel.rating) {
 				hotel.rating = 0;
 			}
 
 			hotel.rating--;
 		}
 
-		$scope.hotels = [{
-			"id": "1",
-			"name": "Taj Westend",
-			"price": 17000,
-			"location": "Race Course Road",
-			"imgUrl": "kVIGWsz.jpg",
-			"url": "http://www.theleela.com/",
-			"availableForBook": false,
-			"description": "Incoherently newt hound ",
-			"area": 40000
-		}, {
-			"id": "2",
-			"name": "Keys Hotel",
-			"price": 8000,
-			"location": "Whitefield",
-			"imgUrl": "OqJMuy5.jpg",
-			"availableForBook": true,
-			"description": "Irresolutely and oh across wherever oh goodness more wallaby a some much reindeer octopus overcast howled far far honey tamarin a ouch inventoried distinct wombat egregious deceptive conservative alas.",
-			"area": 20000
-		}, {
-			"id": "3",
-			"name": "Leela Palace",
-			"price": 20000,
-			"location": "Old Airport Road",
-			"imgUrl": "qA4PCDl.jpg",
-			"availableForBook": true,
-			"description": "Llama crab credible wow reproachfully brave apart dived rugged this hey luscious obediently pert taped smug crud learned thus arousingly or jeepers as before pill save jeez cunning jeez some pointed near house imprecise chose.",
-			"area": 65000
-		}, {
-			"id": "4",
-			"name": "Alila",
-			"price": 20000,
-			"location": "Whitefield",
-			"imgUrl": "qA4PCDl.jpg",
-			"availableForBook": true,
-			"description": "Llama crab credible wow reproachfully brave apart dived rugged this hey luscious obediently pert taped smug crud learned thus arousingly or jeepers as before pill save jeez cunning jeez some pointed near house imprecise chose.",
-			"area": 198000
-		}];
+		$scope.hotels = hp.getHotels();
 	}
 ])
 
 
-angular.module('ha').filter('toFeet', function(){
-	return function(input){
+angular.module('ha').filter('toFeet', function() {
+	return function(input) {
 		return input * 10.3;
 	}
 })
